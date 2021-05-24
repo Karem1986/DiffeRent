41734d8fb41fdae22f78d8ba62d572f39caef1a2
Karem1986 <karem.ortiz@outlook.com>
2021-05-24 13:13:12 +0200

    Prize converted to numeric and added into Num val

--- Data_Cleaning/Pararius_Cleaning.py
+++ Data_Cleaning/Pararius_Cleaning.py
@@ -3,17 +3,19 @@ data = pd.read_csv('pararius_scraped.csv')
 df = pd.DataFrame(
     data, columns=['Address', 'Postcode', 'Size', 'Rooms', 'Prize'])
 print(df.head())
-# Data cleaning-karem:
-# 1: #First remove euro sign, comma and the text in PRIZE
-#example: data['columnName'] = data['columnName'].map(lambda x: x.lstrip('+-').rstrip('aAbBcC'))
-# df['Prize'] = df ['Prize'].map(lambda x: x.lstrip('€').rstrip('per month'))--DONT WORK.
-
-df['Prize'].str.replace(r'\€', '')  # removes any symbols including commas
-df['Prize'].str.replace(r'\D', '')  # removes any letters
-
-# 2.#Convert price to a float
-# df ['Prize'] = pd.to_numeric(df['Prize'], errors='coerce')
-# df = df.replace(np.nan, 0, regex=True)
-df['Prize'].astype(str).astype(float)
-print(df)
-df.dtypes
+# Data cleaning:
+#Prize into Numerical values: 
+df['Prize in €'] = df['Prize in €'].str.replace(',', '') #First we remove the comma's - otherwise our .split() method in the next section will not work properly.
+print('-----------Price column values before preprocessing-----------\n')
+print(df['Prize in €'].unique())
+print('\n\n')
+print('-----------Price column values after removing non number values-----------\n')
+#Apply a function that splits (converts into list) each string value of the Price column, and then only selects the characters that are numbers, and then join those back together to form a string again.
+df['Numerical values'] = df['Prize in €'].apply(lambda x: ''.join(char for char in x.split() if char.isdigit())) #The values of this new column only containt the numbers of the values of the Prize in € column 
+print(df['Numerical values'].unique()) #Checking unique values you see that it worked (although there are missing values)
+print('\n\n')
+print('-----------Price column values after converting column to numeric and labeling missing values-----------\n')
+df['Numerical values'] = pd.to_numeric(df['Numerical values'], errors='coerce') #We convert the columns values to numerical values and give the empty values a nan (not a number) value
+print(df['Numerical values'].unique())
+print('-----------checking the datatype of our new column-----------\n')
+print('The datatype is:',df['Numerical values'].dtypes) #It worked :)
