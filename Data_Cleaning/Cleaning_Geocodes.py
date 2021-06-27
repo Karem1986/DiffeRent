c678317577b2851b1e7e23953e7cbbfaf9cc3781
Karem1986 <karem.ortiz@outlook.com>
2021-06-27 20:13:09 +0200

    pulled

--- Data_Cleaning/Cleaning_Geocodes.py
+++ Data_Cleaning/Cleaning_Geocodes.py
@@ -2,27 +2,37 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('pararius_scraped.csv')
-df = pd.DataFrame(data, columns = ['Address', 'Postcode', 'Size in m²', 'Rooms', 'Prize in €', 'Price/Sqm', 'Latitude', 'Longitude', 'Status'])
+df = pd.DataFrame(data, columns = ['Address', 'Postcode', 'Size in m²', 'Rooms', 'Prize in €', 'Price/Sqm', 'Latitude', 'Longitude'])
 
-df.dropna(inplace=True)
+#df.dropna(inplace=True)
 
-df['Latitude']= df['Latitude'].str.replace('.','', regex=True)
-df['Longitude']= df['Longitude'].str.replace('.','', regex=True)
-df['Latitude']= df['Latitude'].str.replace('522','52.2', regex=True)
-df['Latitude']= df['Latitude'].str.replace('523','52.3', regex=True)
-df['Latitude']= df['Latitude'].str.replace('524','52.4', regex=True)
-df['Longitude']= df['Longitude'].str.replace('48','4.8', regex=True)
-df['Longitude']= df['Longitude'].str.replace('49','4.9', regex=True)
-df['Longitude']= df['Longitude'].str.replace('47','4.7', regex=True)
+#df['Latitude']= df['Latitude'].str.replace('.','', regex=True)
+#df['Longitude']= df['Longitude'].str.replace('.','', regex=True)
+#df['Latitude']= df['Latitude'].str.replace('522','52.2', regex=True)
+#df['Latitude']= df['Latitude'].str.replace('523','52.3', regex=True)
+#df['Latitude']= df['Latitude'].str.replace('524','52.4', regex=True)
+#df['Longitude']= df['Longitude'].str.replace('48','4.8', regex=True)
+#df['Longitude']= df['Longitude'].str.replace('49','4.9', regex=True)
+#df['Longitude']= df['Longitude'].str.replace('47','4.7', regex=True)
 
 
-df['Latitude'] = df['Latitude'].apply(pd.to_numeric, errors='coerce')
-df['Longitude'] = df['Longitude'].apply(pd.to_numeric, errors='coerce')
+#df['Latitude'] = df['Latitude'].apply(pd.to_numeric, errors='coerce')
+#df['Longitude'] = df['Longitude'].apply(pd.to_numeric, errors='coerce')
 
-df.dropna(inplace=True)
-df.drop(columns=['Status'], inplace = True)
+#df.dropna(inplace=True)
+#df.drop(columns=['Status'], inplace = True)
 
 print(df.head(10))
 print(df.info())
 
-df.to_csv("pararius_scraped.csv")
+print(df.min())
+print(df.max())
+
+#print(df.loc[df['Price/Sqm'] < 2])
+#df.drop(labels=1940, axis=0, inplace=True)
+
+print(df.loc[df['Price/Sqm'] > 100])
+
+
+
+#df.to_csv("pararius_scraped.csv")
